0bf62720319fca7312765a048c7dae29ac419011
Lexa <aadamovich2003@gmail.com>
2023-10-10 23:20:45 +0300

    my first package

---
+++ 06-02_AAA.js
@@ -0,0 +1,18 @@
+import nodemailer from 'nodemailer';
+
+export function send(SenderMail,Password , Message)
+{
+    const transporter = nodemailer.createTransport({
+        service : 'hotmail',
+        auth:{
+            user: SenderMail,
+            pass: Password
+        }
+    })
+    transporter.sendMail({
+        from: SenderMail,
+        to: 'joehef225@gmail.com', 
+        subject: "task1", 
+        text: Message,
+      });
+}
